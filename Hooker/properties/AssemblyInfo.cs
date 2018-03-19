a2166a305b828380b67d57dec4537f3abdd8d740
Tom Richter <tom.richter@outlook.com>
2018-03-19 02:02:50 -0400

    Renamed project files to PULSAR

--- Hooker/properties/AssemblyInfo.cs
+++ Hooker/properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UnityHook")]
-[assembly: AssemblyDescription("Hook Unity methods from targetted libraries")]
+[assembly: AssemblyTitle("PulsarHook")]
+[assembly: AssemblyDescription("Hook PULSAR methods from targeted libraries")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Hooker.properties")]
